0cfbfa1633105bc6a017b029bd6946068a1053f0
husisy <zchaozhang@outlook.com>
2022-12-06 10:26:34 +0800

    python/scipy test fft-rfft

--- python/scipy/test_fft.py
+++ python/scipy/test_fft.py
@@ -2,10 +2,13 @@ import numpy as np
 import scipy.fft
 
 hfe = lambda x,y:np.max(np.abs(x-y)/(np.abs(x)+np.abs(y)+1e-3))
+np_rng = np.random.default_rng()
+hf_randc = lambda *size: np_rng.normal(size=size) + 1j*np_rng.normal(size=size)
+
 
 def test_fft(N0=3, N1=5):
     hf_fft_mat = lambda N0: np.exp(-2j*np.pi/N0*(np.arange(N0)[:,np.newaxis]*np.arange(N0)))
-    np0 = np.random.randn(N0, N1) + np.random.randn(N0,N1)*1j
+    np0 = hf_randc(N0, N1)
 
     ret_ = np0 @ hf_fft_mat(N1)
     ret0 = np.fft.fft(np0, axis=1)
@@ -18,7 +21,7 @@ def test_fft(N0=3, N1=5):
 
 def test_ifft(N0=3, N1=5):
     hf_ifft_mat = lambda N0: np.exp(2j*np.pi/N0*(np.arange(N0)[:,np.newaxis]*np.arange(N0)))/N0
-    np0 = np.random.randn(N0, N1) + np.random.randn(N0,N1)*1j
+    np0 = hf_randc(N0, N1)
 
     ret_ = np0 @ hf_ifft_mat(N1)
     ret0 = np.fft.ifft(np0, axis=1)
@@ -51,3 +54,12 @@ def test_fft_dst():
     np2_ = 1/(N0+1)*np.sin(np.pi*np.arange(1,N0+1)[:,np.newaxis]*np.arange(1,N0+1)/(N0+1)) @ np1_
     assert hfe(np1,np1_)<1e-7
     assert hfe(np0,np2_)<1e-7
+
+
+def test_fft_rfft():
+    N0 = 3
+    for N1 in [9,10]:
+        np0 = np_rng.normal(size=(N0,N1))
+        ret_ = scipy.fft.fft(np0, axis=1)
+        ret0 = scipy.fft.rfft(np0, axis=1)
+        assert np.abs(ret_[:,:(N1//2+1)] - ret0).max() < 1e-7
