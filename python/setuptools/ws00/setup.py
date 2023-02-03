2c1f2c68e1a52037d43bccf0b9ad1e538095fd3d
husisy <zchaozhang@outlook.com>
2023-02-03 16:04:56 +0800

    python/setuptools ignore

--- python/setuptools/ws00/setup.py
+++ python/setuptools/ws00/setup.py
@@ -1,13 +1,21 @@
+import os
+import json
 import setuptools
+
+with open(os.path.join('python','mypackage','_package.json'), 'r', encoding='utf-8') as fid:
+    _package = json.load(fid)
+__version__ = _package['version']
 
 with open('README.md', encoding='utf-8') as fid:
     long_description = fid.read()
 
 setuptools.setup(
     name='mypackage', #this is NOT python-module name, one package could include mutiple modules
-    version='0.0.1',
+    version=__version__,
     package_dir={'':'python'},
     packages=setuptools.find_packages('python'),
+    # https://setuptools.pypa.io/en/latest/userguide/datafiles.html
+    package_data={"mypackage": ["*.json"]},
     scripts=['scripts/myscripts'],
     description='an example package',
     long_description=long_description,
