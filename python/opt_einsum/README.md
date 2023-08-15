fecc83b95918356882fd2ae759e14b6c9d625181
husisy <zchaozhang@outlook.com>
2023-08-15 18:08:46 +0800

    python/opt_einsum add contract_expression

--- python/opt_einsum/README.md
+++ python/opt_einsum/README.md
@@ -2,7 +2,7 @@
 
 1. link
    * [github](https://github.com/dgasmith/opt_einsum)
-   * documentation
+   * [documentation](https://dgasmith.github.io/opt_einsum/)
 2. install
    * `conda install -c conda-forge opt_einsum`
    * `pip install opt_einsum`
--- python/opt_einsum/demo00.py
+++ python/opt_einsum/demo00.py
@@ -12,3 +12,18 @@ def demo_opt_einsum_contract_path():
     input_sequence = [y for x in zip(array_list,tmp0) for y in x]
     path,path_info = opt_einsum.contract_path(*input_sequence, [])
     result = opt_einsum.contract(*input_sequence, [], optimize=path)
+
+
+def demo_contract_expression():
+    N0 = 3
+    N1 = 4
+    N2 = 5
+    np_rng = np.random.default_rng()
+    hf0 = lambda *x: np_rng.uniform(-1, 1, size=x)
+    np0 = hf0(N0)
+    np1 = hf0(N0, N1)
+    np2 = hf0(N0, N2)
+    ret_ = opt_einsum.contract(np0, [0], np1, [0,1], np2, [0,2], [1,2])
+    contract_expr = opt_einsum.contract_expression((N0,), (0,), (N0,N1), (0,1), (N0,N2), (0,2), (1,2))
+    ret0 = contract_expr(np0, np1, np2)
+    np.abs(ret_-ret0).max()
