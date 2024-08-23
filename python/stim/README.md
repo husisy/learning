040f03de40af036e67006e04d73a47ff42857fa4
husisy <17511292+husisy@users.noreply.github.com>
2024-08-23 20:59:38 +0800

    python/stim cleanup

--- python/stim/README.md
+++ python/stim/README.md
@@ -4,5 +4,20 @@
    * [github](https://github.com/quantumlib/Stim)
    * [github/docs](https://github.com/quantumlib/Stim/tree/main/doc)
    * [github/stim/gates](https://github.com/quantumlib/Stim/blob/main/doc/gates.md)
+   * [doi-link](https://doi.org/10.22331/q-2021-07-06-497) Stim: a fast stabilizer circuit simulator
 2. install `pip install stim`
-3. target measurement record `target_rec`
+3. concept
+   * target measurement record `target_rec`
+   * DEM: Detector Error Model
+   * phenomenological noise model
+4. subproject: `sinter`
+
+## pymatching
+
+1. link
+   * [github/pymatching](https://github.com/oscarhiggott/PyMatching)
+   * [documentation](https://pymatching.readthedocs.io/en/stable/)
+   * [arxiv-link](https://arxiv.org/abs/2105.13082) PyMatching: A Python package for decoding quantum codes with minimum-weight perfect matching
+   * [arxiv-link](https://arxiv.org/abs/2303.15933) Sparse Blossom: correcting a million errors per core second with minimum-weight matching
+2. install
+   * `pip install pymatching stim`
--- python/stim/draft00.py
+++ python/stim/draft00.py
@@ -6,13 +6,12 @@ circ.append("H", [0])
 circ.append("CNOT", [0, 1]) #bell state
 circ.append("M", [0, 1]) #measure
 # |00> + |11>
-print(repr(circ))
-# stim.Circuit('''
-#     H 0
-#     CX 0 1
-#     M 0 1
-# ''')
-x0 = circ.compile_sampler().sample(shots=5)
+print(circ) #print(repr(circ))
+# circ.diagram('timeline-svg')
+# H 0
+# CX 0 1
+# M 0 1
+x0 = circ.compile_sampler().sample(shots=10)
 # (np,bool,(5,2))
 
 
@@ -20,20 +19,31 @@ circ = stim.Circuit()
 circ.append("H", [0])
 circ.append("CNOT", [0, 1])
 circ.append("M", [0, 1])
-circ.append('DETECTOR', [stim.target_rec(-1), stim.target_rec(-2)])
+circ.append('DETECTOR', [stim.target_rec(-1), stim.target_rec(-2)]) #parity check
 x0 = circ.compile_detector_sampler().sample(shots=5)
-
 
 
 circ = stim.Circuit()
 circ.append("H", [0])
+circ.append("TICK")
 circ.append("CNOT", [0, 1])
 circ.append("X_ERROR", [0, 1], 0.2)
 circ.append("M", [0, 1])
 circ.append('DETECTOR', [stim.target_rec(-1), stim.target_rec(-2)])
-x0 = circ.compile_detector_sampler().sample(shots=5) #one error probability: 0.8*0.2*2
+x0 = circ.compile_detector_sampler().sample(shots=1000) #one error probability: 0.8*0.2*2
 
 
 circ = stim.Circuit.generated("repetition_code:memory", rounds=30, distance=9,
         before_round_data_depolarization=0.03, before_measure_flip_probability=0.01)
 # print(repr(circ))
+
+
+circ = stim.Circuit.generated("repetition_code:memory", rounds=25, distance=9,
+        before_round_data_depolarization=0.04, before_measure_flip_probability=0.01)
+one_sample = circ.compile_sampler().sample(shots=1)[0]
+for k in range(0, len(one_sample), 8):
+    print("".join("1" if e else "_" for e in one_sample[k:k+8]))
+
+one_sample = circ.compile_detector_sampler().sample(shots=1)[0]
+for k in range(0, len(one_sample), 8):
+    print("".join("!" if e else "_" for e in one_sample[k:k+8]))
